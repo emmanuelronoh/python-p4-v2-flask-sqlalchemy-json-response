4a62f15f37ff965e797f990e614d76e1b1f31ed2
emmanuel ronoh <eronoh036@gmail.com>
2024-10-12 11:21:23 +0300

    first commit

--- server/models.py
+++ server/models.py
@@ -1,10 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-# contains definitions of tables and associated schema constructs
 metadata = MetaData()
 
-# create the Flask SQLAlchemy extension
+
 db = SQLAlchemy(metadata=metadata)
 
 # define a model class by inheriting from db.Model.
